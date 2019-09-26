e951e2cec147b71f60a56d5e774710d08ded9c29
Tanner Patterson <tpatterson5492@gmail.com>
2019-09-26 02:10:38 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@ puts "What a nutritious meal!"
 order = "A plate of #{protein} with #{veg1} and #{veg2}."
 puts order
 
-return "What a nutritious meal! #{order}"
+return order
 end
 
 meal_choice("broccoli", "macaroni")
